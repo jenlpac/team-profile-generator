a0e0df80a83bb7120f99a69875af4df87d2382bd
Jenniffer <jenlpac@yahoo.com>
2020-11-06 08:29:23 -0600

    generate manager card

---
+++ src/page-template.js
@@ -0,0 +1,28 @@
+// Create manager card:
+const generateManager = manager => {
+    return `
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Manager Name Goes Here</h2>
+                <h3 class="card-title">Manager Role Goes Here</h2>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID:&nbsp;&nbsp;{Manager ID}</li>
+                    <li class="list-group-item">Email:&nbsp;&nbsp;{Manager email}</li>
+                    <li class="list-group-item">Office #:&nbsp;&nbsp;{Manager office}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+};
+
+// Create engineer card:
+
+
+
+// Create intern card:
+  
+
+
+// Generate entire page:
