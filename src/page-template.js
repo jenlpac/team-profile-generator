6a6d71a4d341c91a18ecf0fa3335311c2565aa3e
Jenniffer <jenlpac@yahoo.com>
2020-11-06 08:34:50 -0600

    generate engineer card/update class

--- src/page-template.js
+++ src/page-template.js
@@ -18,7 +18,24 @@ const generateManager = manager => {
 };
 
 // Create engineer card:
-
+const generateEngineer = engineer => {
+    return `
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Engineer Name Goes Here</h2>
+                <h3 class="card-title">Engineer Role Goes Here</h2>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID:&nbsp;&nbsp;{Engineer ID}</li>
+                    <li class="list-group-item">Email:&nbsp;&nbsp;{Engineer email}</li>
+                    <li class="list-group-item">GitHub Username:&nbsp;&nbsp;{Engineer username}</li>
+                    <li class="list-group-item">GitHub Site:&nbsp;&nbsp;{Engineer github}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+};
 
 
 // Create intern card:
